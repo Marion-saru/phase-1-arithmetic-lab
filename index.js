398f254a9aea16462338b51ee8b5a73d80ff2fc6
Marion Saru <maghangasaru@gmail.com>
2025-04-24 16:47:38 +0300

    Completed optional lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1, num2, num3, num4;
+num1 = 31;
+num2 = 2;
+num3 = 28;
+num4 = 6;
 
-// Write your code here
+let multiply = num1*num2;
+console.log(multiply);
+
+let random = Math.floor(Math.random() *10);
+console.log(random);
+
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(1,2,5,8,10,20,15,6,4.0);
+console.log(max);
